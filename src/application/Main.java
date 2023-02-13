ce2eb3fca2038c5b66858ee5f63cf12e0cfb0e80
VinySales <vinysalesreboucas@hotmail.com>
2023-02-13 02:06:31 -0300

    first commit

---
+++ src/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		method1();
+		
+		System.out.println("End of program");
+		
+	}
+	
+	public static void method1() {
+		System.out.println("======== METHOD 1 START ========");
+		method2();
+		System.out.println("======== METHOD 1 END ========");
+	}
+	
+	public static void method2() {
+		System.out.println("======== METHOD 2 START ========");
+		Scanner scan = new Scanner(System.in);
+		
+		try {
+			String[] vect = scan.nextLine().split(" ");
+			int n = scan.nextInt();
+			
+			System.out.println(vect[n]);
+		} catch (InputMismatchException e) {
+			System.out.println("Only integer number");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position");
+			e.printStackTrace();
+			scan.nextLine();
+		}
+		
+		scan.close();
+		System.out.println("======== METHOD 2 END ========");
+	}
+}
